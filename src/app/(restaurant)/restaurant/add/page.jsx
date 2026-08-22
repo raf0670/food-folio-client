d03d191ac0cb033c7f903b6bca793ade19746347
raf0670 <rafsan0670@gmail.com>
2026-08-22 19:42:41 +0600

    unauthorize enhanced

--- src/app/(restaurant)/restaurant/add/page.jsx
+++ src/app/(restaurant)/restaurant/add/page.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 import RestaurantAddPageHelper from './RestaurantAddPageHelper';
 import { getCurrentUser } from '@/actions/userActions';
 import { redirect } from 'next/navigation';
-import Unauthorized from '@/app/unathorized/page';
 
 const RestaurantAddPage = async () => {
     const user = await getCurrentUser();
     if (!user) {
-        return (
-            <Unauthorized></Unauthorized>
-        )
+        redirect('/unauthorized');
     }
 
     return (
